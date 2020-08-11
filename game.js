38f17c0e1d5b9aefcf9cf7d3428c02e5597ed316
LuckyLuk420 <lpurmom@gmail.com>
2020-08-11 18:38:47 +0200

    fixed coll and added score

--- game.js
+++ game.js
@@ -1,30 +1,29 @@
 var character = document.getElementById("character");
-var block = document.getElementByID("block");
-var game = document.getElementByID("game");
+var block = document.getElementById("block");
+var score = 0
+var check = false
 
 function jump(){
-	checkDead()
-	if (character.classList != "animate"){
-		character.classList.add("animate");
-	}
-	setTimeout(function(){
-		character.classList.remove("animate");
-	}, 500);
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
 }
 
-/*var checkDead = setInterval(function(){
-	let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-	let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-	if (blockLeft<20 && blockLeft>-20 && characterTop>=130){
-		block.style.animation = "none";
-		block.style.display = "none";
-		alert("git  gud!!twelve1");*/
-function checkDead(){
-	let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    	let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    	if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        	block.style.animation = "none";
-        	alert("Game Over.");
-	}
-	setInterval(checkDead(), 10);
-}
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<40 && blockLeft>0 && characterTop>=130){
+        block.style.animation = "none";
+        alert("u suc\ngit gud!!!1twelve");
+        score = 0
+        check = false
+        block.style.animation = "block 1s infinite linear";
+    }else if(blockLeft<=0 && check==false){
+        score++
+        check = true
+        document.getElementById("scoreSpan").innerHTML = score;
+    }
+    if(blockLeft >= 400){check = false}
+}, 10);
